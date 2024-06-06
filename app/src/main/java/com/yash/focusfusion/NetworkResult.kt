6d8be097596e5aa935085afa30cd21ecf3a23d56
Yashveer singh <yashveersingh30401@gmail.com>
2024-06-06 17:59:22 +0530

    NEtwrok Result classes created

--- app/src/main/java/com/yash/focusfusion/NetworkResult.kt
+++ app/src/main/java/com/yash/focusfusion/NetworkResult.kt
@@ -1,4 +1,8 @@
 package com.yash.focusfusion
 
-class NetworkResult {
+sealed class NetworkResult<T>(val data:T?=null, val message:String?=null) {
+
+    class Success<T>(data: T?):NetworkResult<T>(data)
+    class Error<T>(message: String?, data: T?=null):NetworkResult<T>(data, message)
+    class Loading<T>():NetworkResult<T>()
 }
