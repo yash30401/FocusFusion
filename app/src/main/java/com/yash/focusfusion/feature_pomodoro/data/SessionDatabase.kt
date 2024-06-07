0009ac5ccc2085b3e762998b4cd062faacf6f7d5
Yashveer singh <yashveersingh30401@gmail.com>
2024-06-07 16:43:26 +0530

    Database created

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/data/SessionDatabase.kt
@@ -0,0 +1,13 @@
+package com.yash.focusfusion.feature_pomodoro.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.yash.focusfusion.feature_pomodoro.data.local.SessionDao
+import com.yash.focusfusion.feature_pomodoro.data.local.entity.SessionEntity
+
+@Database(entities = [SessionEntity::class], version = 1)
+abstract class SessionDatabase:RoomDatabase() {
+
+    abstract val dao:SessionDao
+
+}
