53c522d1400ee9cad8f2a9b3f83ad360b2a415df
Yashveer singh <yashveersingh30401@gmail.com>
2024-06-09 15:26:36 +0530

    Session State Creted

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/timer_adding_updating_session/SessionEventType.kt
@@ -0,0 +1,6 @@
+package com.yash.focusfusion.feature_pomodoro.presentation.timer_adding_updating_session
+
+enum class SessionEventType {
+    INSERTED,
+    UPDATED
+}
---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/timer_adding_updating_session/SessionState.kt
@@ -0,0 +1,10 @@
+package com.yash.focusfusion.feature_pomodoro.presentation.timer_adding_updating_session
+
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import com.yash.focusfusion.feature_pomodoro.domain.model.Session
+
+data class SessionState(
+    val session: Session?=null,
+    val sessionEventType: SessionEventType,
+    val errorMessage: String?=null
+)
