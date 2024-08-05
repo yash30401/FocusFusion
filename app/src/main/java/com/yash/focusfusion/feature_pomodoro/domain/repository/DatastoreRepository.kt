9a7725d4946f8ff4dc427cbed5252c1805a25fbc
yash30401 <yashveersingh30401@gmail.com>
2024-08-05 22:19:13 +0530

    DatastoreRepository interface created

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/domain/repository/DatastoreRepository.kt
@@ -0,0 +1,17 @@
+package com.yash.focusfusion.feature_pomodoro.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface DatastoreRepository {
+
+    val timeLeftFlow: Flow<Long>
+    val extraTimeFlow: Flow<Int>
+    val continueTimerFlow: Flow<Boolean>
+    val cancelTimeFlow: Flow<Long>
+
+    suspend fun saveTimeLeft(time: Long)
+    suspend fun saveExtraTime(extraTime: Int)
+    suspend fun saveContinueTimer(shouldContinue: Boolean)
+    suspend fun saveCancelTimeLeft(cancelTime: Long)
+
+}
