36175afcb7e0a62eb7c2ab3004f9716cef641e10
yash30401 <yashveersingh30401@gmail.com>
2024-08-06 21:06:01 +0530

    DatastoreRepositoryImpl created

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/data/repository/DatastoreRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.yash.focusfusion.feature_pomodoro.data.repository
+
+
+import com.yash.focusfusion.feature_pomodoro.data.local.datastore.DatastoreManager
+import com.yash.focusfusion.feature_pomodoro.domain.repository.DatastoreRepository
+import kotlinx.coroutines.flow.Flow
+
+
+class DatastoreRepositoryImpl(private val datastoreManager: DatastoreManager) : DatastoreRepository {
+    override val timeLeftFlow: Flow<Long>
+        get() = datastoreManager.timeLeftFlow
+
+    override val extraTimeFlow: Flow<Int>
+        get() = datastoreManager.extraTime
+
+    override val continueTimerFlow: Flow<Boolean>
+        get() = datastoreManager.continueTimerFlow
+
+    override val cancelTimeFlow: Flow<Long>
+        get() = datastoreManager.cancelTimeFlow
+
+    override suspend fun saveTimeLeft(time: Long) {
+        datastoreManager.saveTimeLeft(time)
+    }
+
+    override suspend fun saveExtraTime(extraTime: Int) {
+        datastoreManager.saveExtraTime(extraTime)
+    }
+
+    override suspend fun saveContinueTimer(shouldContinue: Boolean) {
+        datastoreManager.saveContinueTimer(shouldContinue)
+    }
+
+    override suspend fun saveCancelTimeLeft(cancelTime: Long) {
+        datastoreManager.saveCancelTimeLeft(cancelTime)
+    }
+}
