3abc84572dc81cf2df1f84d4b824736928fbbcda
yash30401 <yashveersingh30401@gmail.com>
2024-09-12 13:59:45 +0530

    Insights Screen intial commit

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/InsightsScreen.kt
@@ -0,0 +1,9 @@
+package com.yash.focusfusion.feature_pomodoro.presentation.insights
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InsightsScreen(modifier: Modifier = Modifier) {
+    
+}
