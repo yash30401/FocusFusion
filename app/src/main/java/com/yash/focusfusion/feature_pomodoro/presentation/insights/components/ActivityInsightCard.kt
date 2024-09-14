9f5e9be56b14da5639dff2362e7f89286fe05052
yash30401 <yashveersingh30401@gmail.com>
2024-09-15 02:08:00 +0530

    partially created the activity card

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/components/ActivityInsightCard.kt
@@ -0,0 +1,88 @@
+package com.yash.focusfusion.feature_pomodoro.presentation.insights.components
+
+import android.graphics.drawable.Icon
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.toLowerCase
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yash.focusfusion.R
+import com.yash.focusfusion.feature_pomodoro.domain.model.TaskTag
+import java.util.Locale
+
+@Composable
+fun ActivityInsightCard(
+    @DrawableRes icon: Int,
+    taskTag: TaskTag,
+    totalMinutesForActivity: Int,
+    modifier: Modifier = Modifier
+) {
+
+    val hours by remember { mutableStateOf(totalMinutesForActivity / 60) }
+    val minutes by remember { mutableStateOf(totalMinutesForActivity % 60) }
+
+    Row(modifier = modifier.fillMaxWidth().background(Color.Cyan)
+        .padding(15.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Row(modifier = Modifier.weight(1f), horizontalArrangement =
+        Arrangement.Center) {
+
+
+            Image(
+                painter = painterResource(icon), "",
+                modifier = Modifier.size(25.dp)
+            )
+            Text(text = "${
+                taskTag.name.toLowerCase(Locale.getDefault())
+                    .replaceFirstChar { it.uppercase() }
+            }",
+                fontSize = 20.sp,
+                fontFamily = FontFamily(listOf(Font(R.font.jost_medium)))
+            )
+        }
+
+        VerticalDivider(modifier = Modifier.height(40.dp), thickness = 1.3.dp)
+Row(modifier = Modifier.weight(1f),
+    horizontalArrangement = Arrangement.Center,) {
+    Text(text = "$hours Hrs $minutes min",
+        fontFamily = FontFamily(listOf(Font(R.font.jost_medium)))) }
+
+    }
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ActivityInsightCardPreview() {
+    ActivityInsightCard(
+        R.drawable.books,
+        TaskTag.STUDY,
+        130
+    )
+}
