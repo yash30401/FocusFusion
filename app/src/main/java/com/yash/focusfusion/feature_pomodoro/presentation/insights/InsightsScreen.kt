a84520a9937aa01ff4441855c2d03e63c8676f0b
yash30401 <yashveersingh30401@gmail.com>
2024-09-15 22:25:51 +0530

    padding changes

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/InsightsScreen.kt
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/InsightsScreen.kt
@@ -5,7 +5,10 @@ import android.os.Build
 import androidx.annotation.DrawableRes
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,14 +16,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,10 +52,12 @@ val hoursWorked = listOf(10f, 12f, 20f, 5f, 0f, 4f, 17f)
 @Composable
 fun InsightsScreen(modifier: Modifier = Modifier) {
     Scaffold(modifier = modifier.fillMaxSize()) {
+
+
         LazyColumn(
             modifier = modifier
                 .fillMaxSize()
-                .padding(top = 30.dp, bottom = 40.dp)
+                .padding(top = 50.dp, bottom = 40.dp)
         ) {
 
             item {
@@ -75,7 +91,8 @@ fun InsightsScreen(modifier: Modifier = Modifier) {
                         .padding(start = 10.dp)
                         .padding(vertical = 5.dp),
                     fontFamily = FontFamily(Font(R.font.jost_medium)),
-                    fontSize = 30.sp
+                    fontSize = 30.sp,
+                    color = Color(0xff212121)
                 )
             }
 
