50198caf913b204b9f1af641fde4283acddbf319
yash30401 <yashveersingh30401@gmail.com>
2024-09-18 23:13:23 +0530

    optmized getTimeListInFormattedWay.kt function

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/getTimeListInFormattedWay.kt
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/getTimeListInFormattedWay.kt
@@ -9,12 +9,11 @@ import java.time.format.DateTimeFormatter
 
 @RequiresApi(Build.VERSION_CODES.O)
 fun getTimeListInFormattedWay(sessions: List<Session>): List<String> {
-    val list = mutableListOf<String>()
-    sessions.forEach {
+    val formatter =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault())
+
+    return sessions.map {
         val instant = Instant.ofEpochMilli(it.time)
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-            .withZone(ZoneId.systemDefault())
-        list.add(formatter.format(instant))
+        formatter.format(instant)
     }
-    return list
 }
