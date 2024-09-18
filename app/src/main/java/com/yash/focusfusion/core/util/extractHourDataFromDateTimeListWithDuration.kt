aa1d88da31b79279f24584cd0eaf3fd95c38db07
yash30401 <yashveersingh30401@gmail.com>
2024-09-19 00:51:24 +0530

    change 00 to 0 in extractHourDataFromDateTimeListWithDuration.kt function

--- app/src/main/java/com/yash/focusfusion/core/util/extractHourDataFromDateTimeListWithDuration.kt
+++ app/src/main/java/com/yash/focusfusion/core/util/extractHourDataFromDateTimeListWithDuration.kt
@@ -2,6 +2,7 @@ package com.yash.focusfusion.core.util
 
 fun extractHourDataFromDateTimeListWithDuration(sessionDateTime: List<Pair<String, Int>>): List<Pair<String, Int>> {
     return sessionDateTime.map {
-        it.first.substring(11,13) to it.second
+        if (it.first.substring(11, 13).equals("00")) "0" to it.second else
+            it.first.substring(11, 13) to it.second
     }
 }
