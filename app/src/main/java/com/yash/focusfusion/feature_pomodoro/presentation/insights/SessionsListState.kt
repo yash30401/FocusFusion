f0aead20de73d88fb0460c6f920c970c9f0ef902
yash30401 <yashveersingh30401@gmail.com>
2024-10-26 19:59:27 +0530

    sessionStateList deleted

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/insights/SessionsListState.kt
+++
@@ -1,8 +0,0 @@
-package com.yash.focusfusion.feature_pomodoro.presentation.insights
-
-import com.yash.focusfusion.feature_pomodoro.domain.model.Session
-
-data class SessionsListState(
-    val sessions: List<Session>? = null,
-    val errorMessage: String? = null
-)
