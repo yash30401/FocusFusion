876d462765980792fb31dbb3dd50253eae3ad274
yash30401 <yashveersingh30401@gmail.com>
2025-03-18 21:57:46 +0530

    created HomeScreenViewModel

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/home_screen/HomeScreenViewModel.kt
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/home_screen/HomeScreenViewModel.kt
@@ -1,4 +1,56 @@
 package com.yash.focusfusion.feature_pomodoro.presentation.home_screen
 
-class HomeScreenViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yash.focusfusion.feature_pomodoro.domain.model.Session
+import com.yash.focusfusion.feature_pomodoro.domain.use_case.session_use_case.SessionUseCases
+import com.yash.focusfusion.feature_pomodoro.presentation.insights.InsightsEvent
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class HomeScreenViewModel @Inject constructor(
+    private val sessionUseCases: SessionUseCases,
+) : ViewModel() {
+
+    private val _todaySessions = MutableStateFlow<List<Session>>(emptyList())
+    val todaySessions: StateFlow<List<Session>> get() = _todaySessions
+
+
+    fun onEvent(event: InsightsEvent) {
+        when (event) {
+            is InsightsEvent.DayEvent -> fetchSessionsForDay(event.date)
+
+            is InsightsEvent.MonthEvent,
+            is InsightsEvent.WeekEvent,
+            is InsightsEvent.YearEvent -> {
+                logUnexpectedEvent(event)
+            }
+        }
+    }
+
+
+    private fun fetchSessionsForDay(date: Long) {
+        viewModelScope.launch {
+            try {
+                sessionUseCases.getSessionsForDateUseCase(date).collect { sessions ->
+                    _todaySessions.value = sessions
+                }
+            } catch (e: Exception) {
+                _todaySessions.value = emptyList()
+                 Log.e("HomeScreenViewModel", "Error fetching sessions for date: $date", e)
+            }
+        }
+    }
+
+
+    private fun logUnexpectedEvent(event: InsightsEvent) {
+        // Log.e("HomeScreenViewModel", "Unexpected event received: $event")
+        throw IllegalStateException("Unexpected event received: $event")
+    }
+
+
 }
