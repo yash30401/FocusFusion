2978eb1adc035f99bcd84959d868794bd58266bd
yash30401 <yashveersingh30401@gmail.com>
2025-03-30 21:05:49 +0530

    few changes

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/home_screen/components/TimeDistributionCard.kt
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/home_screen/components/TimeDistributionCard.kt
@@ -1,18 +1,15 @@
 package com.yash.focusfusion.feature_pomodoro.presentation.home_screen.components
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
@@ -27,7 +24,6 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
