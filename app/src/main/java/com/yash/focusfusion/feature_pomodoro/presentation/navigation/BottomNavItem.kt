b9e0f5f2dd1bba135ea7afa4433590789afef123
yash30401 <yashveersingh30401@gmail.com>
2025-04-10 13:18:38 +0530

    bottom nav sealed class created

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/navigation/BottomNavItem.kt
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/navigation/BottomNavItem.kt
@@ -1,3 +1,25 @@
 package com.yash.focusfusion.feature_pomodoro.presentation.navigation
 
-sealed class BottomNavItem()
+import androidx.annotation.DrawableRes
+import com.yash.focusfusion.R
+
+sealed class BottomNavItem(
+    val title: String,
+    @DrawableRes val selectedIcon: Int,
+    @DrawableRes val unselectedIcon: Int,
+    val route: String,
+) {
+    object Home : BottomNavItem(
+        "Home",
+        R.drawable.home_selected,
+        R.drawable.home_unselected,
+        "Home"
+    )
+
+    object Profile:BottomNavItem(
+        "Profile",
+        R.drawable.profile_selected,
+        R.drawable.profile_unselected,
+        "Profile"
+    )
+}
