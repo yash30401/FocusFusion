584c0749a7985e240263388ef1b4e7393aaabb6e
yash30401 <yashveersingh30401@gmail.com>
2025-06-20 14:26:04 +0530

    settings screen design changes

--- app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/settings/SettingsScreen.kt
@@ -1,15 +1,18 @@
 package com.yash.focusfusion.feature_pomodoro.presentation.settings
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -27,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -35,6 +39,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.yash.focusfusion.R
 
 @Composable
@@ -50,42 +55,62 @@ fun SettingsScreen(modifier: Modifier = Modifier) {
 @Composable
 private fun SettingsScreenPreview() {
     Column(modifier = Modifier.fillMaxSize()) {
-        Column(
+
+        Box(
             modifier = Modifier
-                .padding(
-                    horizontal = 20.dp,
-                )
-                .padding(top = 60.dp)
-                .fillMaxWidth()
-                .height(150.dp)
-                .shadow(5.dp, RoundedCornerShape(20.dp))
-                .background(Color(0xffF8F8F8), shape = RoundedCornerShape(20.dp))
-
+                .background(Color.Yellow)
+                .padding(top = 40.dp),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Row(
+
+            Image(
+                painter = painterResource(R.drawable.profile_char),
+                contentDescription = "Profile Char",
                 modifier = Modifier
-                    .padding(top = 30.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "Yashveer",
-                    modifier = Modifier,
-                    textAlign = TextAlign.Center,
-                    fontSize = 30.sp,
-                    fontFamily = FontFamily(Font(R.font.jost_medium)),
-                )
+                    .size(150.dp)
+                    .zIndex(1f)
+                    .offset(y = -10.dp)
 
-                IconButton(onClick = {}) {
-                    Icon(
-                        Icons.Outlined.Edit,
-                        contentDescription = "Edit Name",
-                        tint = Color.Black
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(
+                        horizontal = 20.dp,
                     )
-                }
-            }
+                    .padding(top = 70.dp)
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .shadow(5.dp, RoundedCornerShape(20.dp))
+                    .background(Color(0xffF8F8F8), shape = RoundedCornerShape(20.dp))
 
+            ) {
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 50.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Yashveer",
+                        modifier = Modifier,
+                        textAlign = TextAlign.Center,
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily(Font(R.font.jost_medium)),
+                    )
+
+                    IconButton(onClick = {}) {
+                        Icon(
+                            Icons.Outlined.Edit,
+                            contentDescription = "Edit Name",
+                            tint = Color.Black
+                        )
+                    }
+                }
+
+            }
         }
     }
 }
