a747e736b11d36968b724d4bbf1a8e6abfb66b2e
yash30401 <yashveersingh30401@gmail.com>
2025-07-08 13:28:55 +0530

    streak and greeting implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.yash.focusfusion"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
