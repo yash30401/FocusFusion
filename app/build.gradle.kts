d934ce7e2e7ab4c79eead42334c66b5bc5805d9f
yash30401 <yashveersingh30401@gmail.com>
2025-08-10 22:11:14 +0530

    few gradle changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 android {
     namespace = "com.yash.focusfusion"
     compileSdk = 35
+    buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "com.yash.focusfusion"
