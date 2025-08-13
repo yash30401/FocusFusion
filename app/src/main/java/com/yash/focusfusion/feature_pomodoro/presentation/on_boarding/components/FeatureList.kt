4f32fba8da69ba52ed8b6dafc271278d30ba1ef7
yash30401 <yashveersingh30401@gmail.com>
2025-08-13 14:42:32 +0530

    onboardingScreen 1 design completed

---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/on_boarding/components/FeatureList.kt
@@ -0,0 +1,78 @@
+package com.yash.focusfusion.feature_pomodoro.presentation.on_boarding.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yash.focusfusion.R
+
+@Composable
+fun FeatureList(
+    @DrawableRes image: Int,
+    featureText: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .padding(top = 10.dp)
+            .background(
+                color = Color(0xffF9F9F9),
+                shape = RoundedCornerShape(20.dp)
+            )
+            .padding(10.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.White, shape = CircleShape)
+                .padding(2.dp)
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = "$featureText",
+                alignment = Alignment.Center,
+            )
+        }
+        Text(
+            text = featureText, fontSize = 18.sp,
+            fontFamily = FontFamily(Font(R.font.roboto_regular)), modifier =
+                Modifier.padding(start = 10.dp)
+        )
+
+    }
+}
+
+@Preview(
+    showBackground = true, showSystemUi = true,
+    backgroundColor = 0xffFFFFFF
+)
+@Composable
+private fun FeatureListPrev() {
+    FeatureList(
+        R.drawable.timer_clock,
+        "Finish Work Without Distractions"
+    )
+}
+
+data class FeatureListData(
+    @DrawableRes val image: Int,
+    val featureText: String,
+)
---
+++ app/src/main/java/com/yash/focusfusion/feature_pomodoro/presentation/on_boarding/screens/OnBoardingScreen1.kt
@@ -0,0 +1,102 @@
+package com.yash.focusfusion.feature_pomodoro.presentation.on_boarding.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yash.focusfusion.R
+import com.yash.focusfusion.feature_pomodoro.presentation.on_boarding.OnBoardingUiEvent
+import com.yash.focusfusion.feature_pomodoro.presentation.on_boarding.components.FeatureList
+import com.yash.focusfusion.feature_pomodoro.presentation.on_boarding.components.FeatureListData
+
+@Composable
+fun OnBoardingScreen1(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun OnBoardingScreen1Prev() {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .padding(20.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(
+                text = "focus\nfusion",
+                fontFamily = FontFamily(Font(R.font.fugaz_one_regular)),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 20.dp),
+                fontSize = 50.sp,
+                color = Color(0xffFF8D61)
+            )
+
+            LazyColumn(modifier = Modifier.padding(top = 20.dp)) {
+                items(feautreListItem) { item ->
+                    FeatureList(item.image, item.featureText)
+                }
+            }
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xffFF8D61)
+            ),
+            shape = RoundedCornerShape(32.dp)
+        ) {
+            Text(
+                text = "Get Started",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
+    }
+}
+
+val feautreListItem = listOf<FeatureListData>(
+    FeatureListData(
+        R.drawable.timer_clock,
+        "Finish Work Without Distractions"
+    ),
+    FeatureListData(
+        R.drawable.take_break,
+        "Pause To Power Up"
+    ),
+    FeatureListData(
+        R.drawable.streak_icon,
+        "Track Your Streaks And Wins"
+    ),
+    FeatureListData(
+        R.drawable.chart_icon,
+        "Turn Consistency Into Results"
+    )
+)
