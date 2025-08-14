888fcbc3fd861451fd0012f5df6bbed8ff6f151a
yash30401 <yashveersingh30401@gmail.com>
2025-08-15 00:53:24 +0530

    r8 enabled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,7 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# Keep all data model/entity classes used by Room
+-keep class com.yash.focusfusion.feature_pomodoro.data.local.** { *; }
+
