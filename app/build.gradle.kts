32f7c0cd7328eccfe5da15cf2fd0b9a34bed3e0c
yash30401 <yashveersingh30401@gmail.com>
2025-08-18 23:33:01 +0530

    final change version codes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.yash.focusfusion"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
